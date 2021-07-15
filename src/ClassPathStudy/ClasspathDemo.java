07ce937cc95eea151c279e570e570e97ff4bd480
GyeomFka <clazziquai01@gmail.com>
2021-07-15 21:40:51 +0900

    5일차 javaStudy  - 접근제어자를 왜 써야하는가  - 부연개념 : 유지보수 측면에서도 바라봐도 좋을것같다.  public  라면 고치고자 할때 프로젝트 전반적으로 봐야한다.  protected 라면 유지보수 측면이 줄어든다 (class < package)  private ? -> 클래스 내부에서만 고치면 된다.

---
+++ src/ClassPathStudy/ClasspathDemo.java
@@ -0,0 +1,14 @@
+package ClassPathStudy;
+
+class Item {
+    public void print() {
+        System.out.println("HELLO CLASSPATHSTUDY");
+    }
+}
+
+class ClasspathDemo {
+    public static void main(String[] args) {
+        Item i = new Item();
+        i.print();
+    }
+}
